1a0dd88756f1ab2be0a4d441fa40e5b4d67f125e
Sergey <sivak.93@bk.ru>
2023-02-22 15:34:22 +0500

    Add Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B. Реализовать через функции.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Данные для ввода и вывода 
+int valueA = InputInt("Введите число: ");
+int valueB = InputInt("Введите степень: ");
+degree(valueA, valueB);
+
+// Ввод чисел
+int InputInt(string prompt)
+{
+    System.Console.Write($"{prompt} : ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Подсчет степени
+void degree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine($"{b} степень числа {a} равна {result}");
+
+}
+
+
+
+
