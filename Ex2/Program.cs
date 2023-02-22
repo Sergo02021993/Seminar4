210c2d29797698ced47d13d19b27ce22867be3a4
Sergey <sivak.93@bk.ru>
2023-02-22 15:34:38 +0500

    Add Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// Реализовать через функции.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Данные для ввода и вывода 
+int value = InputInt("Введите число: ");
+int length = numberLength(value);
+resultSum(value, length);
+
+// Ввод чисел
+int InputInt(string prompt)
+{
+    System.Console.Write($"{prompt}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Подсчет цифр в числе
+int numberLength(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция подсчетта суммы всех чисел в веденном значении
+void resultSum(int value, int length)
+{
+    int sum = 0;
+    for (int i = 1; i <= length; i++)
+    {
+        sum += value % 10;
+        value /= 10;
+    }
+    Console.WriteLine($"Сумма всех чисел в веденном значении равна {sum}.");
+}
+
